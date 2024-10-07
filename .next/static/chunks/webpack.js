332a13d61beb4dc004db7dd795afaf4a9e015a89
onehowon <flash0221@naver.com>
2024-10-07 14:14:27 +0900

    chore: iframe 방식으로 변환

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "828b756141b5e04c"; }
+/******/ 		__webpack_require__.h = function() { return "0e6e2bcf3c305c72"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
